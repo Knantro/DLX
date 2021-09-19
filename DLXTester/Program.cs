a788e939b871ffd7db1b2bc7bdf24fa336d75386
Knantro <3jrcfql478>
2021-09-19 17:12:07 +0300

    Added folder tests.

--- DLXTester/Program.cs
+++ DLXTester/Program.cs
@@ -5,11 +5,11 @@ namespace DLXTester {
         private static void Main() {
             var obj = new FourLinkedList();
             var args = new[,] {
-                {0, 1, 1, 0, 1 },
-                {1, 0, 1, 0, 0 },
-                {0, 0, 0, 1, 1 },
-                {0, 1, 1, 1, 1 },
-                {1, 0, 0, 1, 0 }
+                { 0, 1, 1, 0, 1 },
+                { 1, 0, 1, 0, 0 },
+                { 0, 0, 0, 1, 1 },
+                { 0, 1, 1, 1, 1 },
+                { 1, 0, 0, 1, 0 }
             };
             obj.BuildList(args);
             obj.Describe();
---
+++ DLXTests/FourLinkedListTests.cs
@@ -0,0 +1,17 @@
+using DLX;
+using Xunit;
+
+namespace DLXTests {
+    public class FourLinkedListTests {
+        private FourLinkedList obj;
+
+        private void Reset() {
+            obj = new FourLinkedList();
+        }
+        
+        [Fact]
+        public void Meth() {
+            
+        }
+    }
+}
